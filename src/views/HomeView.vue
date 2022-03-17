c7c51b42b25126871d69b0594ce62a3febd358c3
Luka Saler <satch.lucas@yahoo.com>
2022-03-17 23:23:50 +0100

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<h1>Homepage</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, aperiam accusantium. Sunt reprehenderit alias modi cum accusantium rem ad perspiciatis harum at corrupti aliquam possimus earum itaque officia repudiandae esse commodi voluptates, reiciendis praesentium atque optio quam. Nisi dolor ipsum sequi laborum necessitatibus omnis reprehenderit laboriosam vitae officia dolorem maiores aliquid placeat laudantium nulla molestiae, autem, in consequatur architecto, facilis officiis repudiandae molestias. Debitis suscipit iusto sapiente, asperiores odit officiis nostrum eius laborum assumenda? Ut unde, quia nihil rem ipsam aliquid! Nemo perspiciatis tempore veritatis eos officia eius debitis. Eligendi aliquam a exercitationem iste quasi reprehenderit ducimus tempore fugiat excepturi.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, aperiam accusantium. Sunt reprehenderit alias modi cum accusantium rem ad perspiciatis harum at corrupti aliquam possimus earum itaque officia repudiandae esse commodi voluptates, reiciendis praesentium atque optio quam. Nisi dolor ipsum sequi laborum necessitatibus omnis reprehenderit laboriosam vitae officia dolorem maiores aliquid placeat laudantium nulla molestiae, autem, in consequatur architecto, facilis officiis repudiandae molestias. Debitis suscipit iusto sapiente, asperiores odit officiis nostrum eius laborum assumenda? Ut unde, quia nihil rem ipsam aliquid! Nemo perspiciatis tempore veritatis eos officia eius debitis. Eligendi aliquam a exercitationem iste quasi reprehenderit ducimus tempore fugiat excepturi.</p>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+	name: 'HomeView',
+	components: { }
 }
 </script>
